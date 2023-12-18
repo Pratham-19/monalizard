ef84adc92ea8fd00ba1074b1eceb1e13f1a021f3
Pratham <prathamsinghal2003@gmail.com>
2023-12-18 19:33:16 +0530

    feat: add landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,113 +1,182 @@
-import Image from 'next/image'
+import Link from "next/link";
+import MaxWidthWrapper from "./_components/MaxWidthWrapper";
+import Image from "next/image";
+import LandingNavbar from "./_components/Navbar/LandingNav";
+import Footer from "./_components/Footer";
 
 export default function Home() {
+  const data: {
+    icon: string;
+    title: string;
+  }[] = [
+    {
+      icon: "/l1.svg",
+      title: "Meet new friends",
+    },
+    {
+      icon: "/l2.svg",
+      title: "Earn rewards",
+    },
+    {
+      icon: "/l3.svg",
+      title: "Collect NFTs",
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <MaxWidthWrapper className="flex flex-col items-center py-4 relative overflow-x-hidden">
+      <LandingNavbar />
+      <main className="mt-7 w-[90%] mx-auto">
+        <div className="bg-[#FFFBF8]/[0.3] border rounded-[50px] h-[63vh] flex backdrop-blur-sm pl-5 pr-7 mt-7 ">
+          <div className="w-[35%] flex justify-center items-center ">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/hero.jpg"
+              alt="hero"
+              width={1021}
+              height={1276}
+              className=" w-[80%] h-[80%]"
             />
-          </a>
+          </div>
+          <section className=" w-[75%] flex flex-col justify-around items-center">
+            <h2 className="text-7xl font-bold">Discover Web3</h2>
+            <h3 className="italic text-2xl text-center">
+              Explor Dapp with community solved puzzles.
+            </h3>
+            <div className="grid grid-cols-3 gap-3 ">
+              {data.map((item) => (
+                <div
+                  key={item.title}
+                  className="bg-[#200F00] flex justify-center items-center  space-x-2 rounded-xl p-2 hover:scale-95 transition-taransform duration-300 backdrop-blur-sm shadow-md"
+                >
+                  <Image
+                    src={item.icon}
+                    alt="hero"
+                    width={60}
+                    height={60}
+                    className="p-1"
+                  />
+                  <h2 className="text-[#EFB359]">{item.title}</h2>
+                </div>
+              ))}
+            </div>
+          </section>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <div className="flex h-[38vh] my-10 ">
+          <section>
+            <Image
+              src="/G.png"
+              alt="hero"
+              width={868}
+              height={952}
+              className="aspect-auto h-full px-8"
+            />
+          </section>
+          <h2 className="italic text-2xl text-left my-auto font-semibold leading-[4rem]">
+            “Get puzzle pieces from time to time or earn them by completing a
+            task. But watch out, your time run fast. Make frens to complete
+            them, don&apos;t be last”
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex justify-between h-[38vh] my-12">
+          <section className="w-[30%] flex flex-col justify-between">
+            <div className="bg-[#200F00] rounded-xl p-3 flex justify-center items-center space-x-2 h-[45%] text-[#EFB359]">
+              <Image
+                src="/participants-y.svg"
+                alt="hero"
+                width={40}
+                height={40}
+                className="h-10 w-10"
+              />
+              <h2>10 Active Puzzles</h2>
+            </div>
+            <div className="bg-[#200F00] rounded-xl p-3 flex justify-center items-center space-x-2 h-[45%] text-[#EFB359]">
+              <Image
+                src="/quest-y.svg"
+                alt="hero"
+                width={40}
+                height={40}
+                className="w-10 h-10"
+              />
+              <h2>50 Active Lizards</h2>
+            </div>
+          </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <Link
+            href="/user/signup"
+            className="w-[30%] relative rounded-2xl overflow-hidden cursor-pointer hover:scale-[1.03] transition-transform duration-300"
+          >
+            <Image
+              src="/user-lizard.jpg"
+              alt="promoter"
+              width={1024}
+              height={1024}
+              className="object-cover w-full h-full"
+            />
+            <div className="absolute inset-x-0 bottom-4 flex justify-center items-center bg-black rounded-xl w-[80%] mx-auto text-white space-x-2 py-2 opacity-80">
+              <h2 className="uppercase text-lg font-semibold leading-loose">
+                Start puzzling
+              </h2>
+            </div>
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-[#fe8c27] opacity-[0.15] " />
+          </Link>
+          <Link
+            href="/promoter/signup"
+            className="w-[30%] relative rounded-2xl overflow-hidden cursor-pointer hover:scale-[1.03] transition-transform duration-300"
+          >
+            <Image
+              src="/sponsor-lizard.jpg"
+              alt="promoter"
+              width={1024}
+              height={1024}
+              className="object-cover w-full h-full"
+            />
+            <div className="absolute inset-x-0 bottom-4 flex justify-center items-center bg-black rounded-xl w-[80%] mx-auto text-white space-x-2 py-2 opacity-80">
+              <h2 className="uppercase text-lg font-semibold leading-loose">
+                Promoter Project
+              </h2>
+            </div>
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-[#fe8c27] opacity-[0.15] " />
+          </Link>
+        </div>
+        <div className="flex h-[38vh] my-10 ">
+          <section>
+            <Image
+              src="/D.png"
+              alt="hero"
+              width={808}
+              height={920}
+              className="aspect-auto h-full  pl-8 pr-16"
+            />
+          </section>
+          <h2 className="italic text-2xl text-left my-auto font-semibold leading-[4rem]">
+            “Don&apos;t struggle to get users on your dApp, With a puzzle
+            you&apos;ll get a ton ASAP, Create a campaign now, easy and fast”
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        </div>
+      </main>
+      <Footer className="w-full bg-[hsl(var(--primary))] flex justify-between items-center h-16 rounded-2xl px-3 py-5 " />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Image
+        src="/a1.png"
+        alt="l1"
+        width={800}
+        height={800}
+        className="absolute -top-10 -right-5 -z-10 opacity-10"
+      />
+      <Image
+        src="/a3.png"
+        alt="l1"
+        width={800}
+        height={800}
+        className="absolute bottom-10 -right-[300px] -z-10 opacity-10"
+      />
+      <Image
+        src="/a2.png"
+        alt="l1"
+        width={800}
+        height={800}
+        className="absolute top-[500px] -left-72 -z-10 opacity-10"
+      />
+    </MaxWidthWrapper>
+  );
 }
